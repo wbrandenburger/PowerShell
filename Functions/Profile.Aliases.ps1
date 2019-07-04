45e66c93279966d521977a86e410e75d615b268d
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2019-07-04 08:51:52 +0200

    added module aliases

--- Functions/Profile.Aliases.ps1
+++ Functions/Profile.Aliases.ps1
@@ -7,21 +7,28 @@
 
     # define aliases for specific function
     [PSCustomObject[]] $Script:PSAlias = @(
-        # aliases for functions to manage repositories
-        [PSCustomObject] @{Name="cdrep"; Value="Set-LocationRepository"; Tag="Repository" },
-        [PSCustomObject] @{Name="cppsrep"; Value="Copy-PSModuleFromRepository"; Tag="Repository"},
-        [PSCustomObject] @{Name="gitrep"; Value="Start-RepositoryWeb"; Tag="Repository"},
-        [PSCustomObject] @{Name="lsrep"; Value="Get-Repository"; Tag="Repository"},
-        [PSCustomObject] @{Name="vsrep"; Value="Start-RepositoryVSCode"; Tag="Repository"},
-        
-        # aliases for functions to manage projects
-        [PSCustomObject] @{Name="cdproj"; Value="Set-LocationProject"; Tag="Project"},
-        [PSCustomObject] @{Name="lsproj"; Value="Get-Project"; Tag="Project"},
-        [PSCustomObject] @{Name="vsproj"; Value="Start-ProjectVSCode"; Tag="Project"},
 
-        # aliases for common functions
-        [PSCustomObject] @{Name="psalias"; Value="Get-PSAlias"; Tag="Common"},
-        [PSCustomObject] @{Name="refreshps"; Value="Restart-PSSession";Tag="Common"}
+    # aliases for functions to manage repositories
+    [PSCustomObject] @{Name="cdrep"; Value="Set-LocationRepository"; Tag="Repository" }
+    [PSCustomObject] @{Name="cppsrep"; Value="Copy-PSModuleFromRepository"; Tag="Repository"}
+    [PSCustomObject] @{Name="webrep"; Value="Start-RepositoryWeb"; Tag="Repository"}
+    [PSCustomObject] @{Name="lsrep"; Value="Get-Repository"; Tag="Repository"}
+    [PSCustomObject] @{Name="vsrep"; Value="Start-RepositoryVSCode"; Tag="Repository"}
+        
+    # aliases for functions to manage modules
+    [PSCustomObject] @{Name="lspsm"; Value="Get-PSModule"; Tag="Module"}
+    [PSCustomObject] @{Name="mkpsm"; Value="Import-PSModule"; Tag="Module"}
+    [PSCustomObject] @{Name="rmpsm"; Value="Remove-PSModule"; Tag="Module"}
+    [PSCustomObject] @{Name="webpsm"; Value="Start-PSModuleWeb"; Tag="Module"}
+    
+    # aliases for functions to manage projects
+    [PSCustomObject] @{Name="cdproj"; Value="Set-LocationProject"; Tag="Project"},
+    [PSCustomObject] @{Name="lsproj"; Value="Get-Project"; Tag="Project"},
+    [PSCustomObject] @{Name="vsproj"; Value="Start-ProjectVSCode"; Tag="Project"},
+
+    # aliases for common functions
+    [PSCustomObject] @{Name="psalias"; Value="Get-PSAlias"; Tag="Common"},
+    [PSCustomObject] @{Name="refreshps"; Value="Restart-PSSession";Tag="Common"}
     )
     
     $Script:PSAlias | ForEach-Object {
