be028665ab29c6075f0385ff1bf419a47780b444
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2019-08-15 14:53:23 +0200

    Added a function for the output of url of git repositories

--- Functions/Profile.Repository.ps1
+++ Functions/Profile.Repository.ps1
@@ -66,6 +66,50 @@ function Get-ActiveRepositoryCollection {
     }
 }
 
+
+#   function -----------------------------------------------------------------
+# ----------------------------------------------------------------------------
+function Get-Repository {
+    
+    [CmdletBinding(PositionalBinding=$True)]
+    
+    [OutputType([PSCustomObject])]
+
+    Param (
+        [ValidateSet([ValidateRepositoryAlias])]
+        [Parameter(Position=1, Mandatory=$True)]
+        [System.String] $Name,
+
+        [Parameter()]
+        [Switch] $Fork
+    )
+
+    Process{
+        
+        $repositorySpecies = Select-Project -Name $Name -Property Repository -Type Repository
+
+        if ($repositorySpecies -eq "Collection"){
+            Write-FormatedError -Message "User specification is a collection of repositories, which does not contain a url for a single repository."
+            return Get-ProfileProject Repository
+        }
+
+        $property = "GitHub"
+        if ($type -eq "Repository"){
+            if ($Fork) {
+                $property = "Fork"
+            }
+        }
+       
+        $selection =  Select-Project -Name $Name -Property $property -Type Repository
+ 
+        if ($selection) { return $selection }
+        else {
+            Write-FormatedError -Message "No valid repository was found."
+            return Get-ProfileProject
+        }
+    }
+
+}
 #   function -----------------------------------------------------------------
 # ----------------------------------------------------------------------------
 function Start-RepositoryCollection {
