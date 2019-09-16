aac5c85c30ff55e8df3d79ec67a73358322080dd
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2019-09-16 17:01:19 +0200

    install command for sciprofile

--- InstallSciProfile/install.ps1
+++ InstallSciProfile/install.ps1
@@ -6,13 +6,24 @@
 # ---------------------------------------------------------------------------
 $path = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 $pwsh_path = Split-Path -Path $path  -Parent
+
 $settings_file = Join-Path -Path $path -ChildPath "sciprofile.json"
 $import_file = Join-Path -Path $path -ChildPath "import.json"
 $refresh_cmd = Join-Path -Path $path -ChildPath "RefreshEnv.cmd"
 $settings = Get-Content -Path $settings_file | ConvertFrom-Json
 
+$sciprofile_path = Join-Path -Path $pwsh_path -ChildPath "SciProfile"
+$modules_path = Join-Path -Path $pwsh_path -ChildPath "Modules"
+$sciprofile_config_file = Join-Path -Path $settings.XDG_CONFIG_HOME -ChildPath "sciprofile\config.ini"
+$sciprofile_import_file = Join-Path -Path $settings.XDG_CONFIG_HOME -ChildPath "sciprofile\config\import.json"
+
 #   environment -------------------------------------------------------------
 # ---------------------------------------------------------------------------
+$env_module_path = [System.Environment]::GetEnvironmentVariable("PSModulePath", "process")
+if (-not $($env_module_path -match $($modules_path -replace "\\", "\\"))){
+    [System.Environment]::SetEnvironmentVariable("PSModulePath", $modules_path, "User")
+}
+
 [System.Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", $settings.XDG_CONFIG_HOME, "User")
 [System.Environment]::SetEnvironmentVariable("XDG_CONFIG_DIRS", "$($settings.XDG_CONFIG_DIRS);", "User")
 
@@ -28,13 +39,8 @@ Install-Module SciProfile -Scope CurrentUser -AllowClobber -Force
 
 Start-Process -FilePath pwsh -Wait -NoNewWindow
 
-#   settings ----------------------------------------------------------------
+#   configuration -----------------------------------------------------------
 # ---------------------------------------------------------------------------
-$sciprofile_path = Join-Path -Path $pwsh_path -ChildPath "SciProfile"
-$modules_path = Join-Path -Path $pwsh_path -ChildPath "Modules"
-$sciprofile_config_file = Join-Path -Path $settings.XDG_CONFIG_HOME -ChildPath "sciprofile\config.ini"
-$sciprofile_import_file = Join-Path -Path $settings.XDG_CONFIG_HOME -ChildPath "sciprofile\config\import.json"
-
 Get-IniContent -FilePath $sciprofile_config_file | Set-IniContent -NameValuePairs @{"module-dir"=$modules_path} -Sections "sciprofile" |  Set-IniContent -NameValuePairs @{"scripts-dir"=$sciprofile_path } -Sections "user" | Out-IniFile $sciprofile_config_file -Force -Pretty -Loose
 
 if (-not $(Test-Path -Path $sciprofile_import_file )) {
