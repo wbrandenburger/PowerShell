3a14f9218d69ef8a3528c0428f2d143684f0f0d3
Wolfgang Brandenburger <w.brandenburger@unibw.de>
2020-09-21 19:07:37 +0200

    added cocojson2singlejson

--- SciProfile/project.ps1
+++ SciProfile/project.ps1
@@ -34,3 +34,36 @@ function Global:Get-Repository {
 
     return $result | Format-Table Name, Alias, Url
 }
+
+
+#   function ----------------------------------------------------------------
+# ---------------------------------------------------------------------------
+function Global:CocoJson2SingleJson {
+
+    [CmdletBinding(PositionalBinding=$True)]
+    
+    [OutputType([System.Object])]
+
+    Param (
+        [System.String] $FileName="train_anno.json",
+        [System.String] $DstDir="label_json",
+        [System.String] $SrcDir="A:\Datasets\GAOFEN\train"  
+    )
+
+    $src_file_path = Join-Path -Path $SrcDir -ChildPath $FileName
+    $dst_path = Join-Path -Path $SrcDir -ChildPath $DstDir
+
+    $obj = Get-Content -Path $src_file_path | ConvertFrom-Json
+    # Write-Host $obj.images
+    $obj.images | ForEach-Object {
+        # Write-Host $_.file_name
+        $image_id = $_.id
+        $file_name = $_.file_name
+        $obj_anno = $obj.annotations | Where-Object{$_.image_id -eq $image_id}
+        # Write-Host $obj_anno
+
+        $dst_file_path = Join-Path -Path $dst_path -ChildPath ((Split-Path $file_name -leafBase) + ".json")
+        Write-FormattedProcess -Message "Write file $dst_file_path"
+        $obj_anno | ConvertTo-Json | Out-File $dst_file_path
+    }
+}
