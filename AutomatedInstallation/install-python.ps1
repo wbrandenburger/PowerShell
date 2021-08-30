cd17659b5cbca543fda69ecf8ff8aac3dbbc9c13
Wolfgang Brandenburger <w.brandenburger@unibw.de>
2021-08-30 23:30:10 +0200

    changed gdal environment variables

--- AutomatedInstallation/install-python.ps1
+++ AutomatedInstallation/install-python.ps1
@@ -9,7 +9,7 @@ $url_python = 'https://www.python.org/ftp/python/3.7.9/python-3.7.9-amd64.exe' #
 # $url_python = https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe # 3.9.6
 
 $result = getInstaller -Path $msi_python -Url $url_python -Identifier $id
-if ($env:AutoInstall -and $result){. $msi_python }
+if ($env:AutoInstall -and -not $result){. $msi_python }
 
 if ($env:AutoSetEnvironment){
     Set-EnvVariable "PYTHONHOME" -Value ( $env:LOCALAPPDATA | Join-Path -ChildPath "Programs" | Join-Path -ChildPath "Python" | Join-Path -ChildPath "Python37")  -Scope "user"
--- AutomatedInstallation/install-tcp_ip.ps1
+++ AutomatedInstallation/install-tcp_ip.ps1
@@ -7,4 +7,4 @@ $msi_tcp_ip = Join-Path -Path $temp_path -ChildPath "tcp-ip-manager-4.1.1.29.exe
 $url_tcp_ip= 'http://downloads.sourceforge.net/tcpipmanager/TCP_IP_Manager_v4.1.1.29_x64_Setup.exe'
 
 $result = getInstaller -Path $msi_tcp_ip -Url $url_tcp_ip -Identifier $id
-if ($env:AutoInstall -and $result) { . $msi_tcp_ip }
+if ($env:AutoInstall -and -not $result) { . $msi_tcp_ip }
