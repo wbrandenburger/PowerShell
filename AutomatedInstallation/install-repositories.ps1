b9eeed9c20b70ae2a1a919c57d9b82d3184f6f48
Wolfgang Brandenburger <w.brandenburger@unibw.de>
2021-09-06 18:53:43 +0200

    added repositories

--- AutomatedInstallation/install-repositories.ps1
+++ AutomatedInstallation/install-repositories.ps1
@@ -2,3 +2,13 @@ $repo_path = "A:\Repositories"
 if (-not (Test-Path -Path $repo_path)) {
     New-Item -Path $repo_path -ItemType Directory
 }
+
+$tempFolderPath = Join-Path $Env:Temp $(New-Guid)
+New-Item -Type Directory -Path $tempFolderPath | Out-Null
+
+git clone https://github.com/wbrandenburger/PSVirtualEnvConfig $tempFolderPath 
+Move-Item $tempFolderPath A:\VirtualEnv --Force
+
+# git clone https://github.com/wbrandenburger/EvalObjD A:\Repositories\EvalObjD 
+# git clone https://github.com/wbrandenburger/ObjGis A:\Repositories\ObjGis
+# git clone https://github.com/wbrandenburger/Strato A:\Repositories\WebAPI-Strato
